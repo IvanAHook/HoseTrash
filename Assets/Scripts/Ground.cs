d25b5cd9f2070d802157b3f92977618d1036088e
Ivan Höök <ivan.a.hook@gmail.com>
2016-08-15 20:11:23 +0200

    some hose stuff

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ground : MonoBehaviour {
+
+	private bool _watered;
+
+	void OnParticleCollision(GameObject other)
+	{
+		if (!_watered)
+		{
+			_watered = true;
+			GetComponent<SpriteRenderer>().color = Color.blue;
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -5,33 +5,80 @@ public class PlayerInput : MonoBehaviour
 {
 	public KeyCode Left;
 	public KeyCode Right;
+	public KeyCode Jump;
 	public KeyCode Hose;
 
-	private RigidBody2D rigidBody;
+	private float _speed = 5;
+	private float _jumpForce = 240;
+	private bool _facingRight = true;
+
+	[SerializeField]private ParticleSystem _water;
+
+	private Rigidbody2D _rigidBody;
+
+	void Awake()
+	{
+		_water.Stop();
+	}
 
 	void Start ()
 	{
-		rigidBody = GetComponent<RigidBody2D>();
+		_rigidBody = GetComponent<Rigidbody2D>();
 	}
 	
-	void Update ()
+	void FixedUpdate ()
 	{
-		var currentVelocity = rigidBody.velocity;
+		var currentVelocity = _rigidBody.velocity;
 		currentVelocity.x = 0;
-		if (Input.GetKeyDown(Left))
+		//_water.Stop ();
+		if (Input.GetKey(Left))
 		{
 			//IMovement.MoveLeft();
-			currentVelocity.x = -1;
+			currentVelocity.x = -1 * _speed;
 		}
-		if (Input.GetKeyDown(Right))
+		if (Input.GetKey(Right))
 		{
 			//IMovement.MoveRight();
-			currentVelocity.x = 1;
+			currentVelocity.x = 1 * _speed;
+		}
+		if (Input.GetKeyDown(Jump))
+		{
+			_rigidBody.AddForce(new Vector2 (0, _jumpForce));
 		}
 		if (Input.GetKeyDown(Hose))
 		{
 			//Hose
+			_water.Play();
 		}
-		rigidBody.velocity = currentVelocity;
+		if (Input.GetKeyUp(Hose))
+		{
+			_water.Stop();
+		}
+		_rigidBody.velocity = currentVelocity;
 	}
+
+	private void Flip()
+	{
+		_facingRight = !_facingRight;
+		var scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+
+	private bool IsGrounded()
+	{
+		RaycastHit hit;
+		float distance = 1f;
+		Vector3 dir = new Vector3(0, -1);
+
+		if(Physics.Raycast(transform.position, dir, out hit, distance))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
 }
